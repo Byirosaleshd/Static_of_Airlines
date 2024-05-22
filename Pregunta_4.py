e50e10e7444a3b45b435226ff878fd2f01094876
Byirosaleshd <68984100+Byirosaleshd@users.noreply.github.com>
2024-05-21 22:43:55 -0400

    Organizacion de la pregunta 4
    Organizace la pregunta 4 en un archivo jupyter notebook.

--- Pregunta_4.py
+++ Pregunta_4.py
@@ -4,6 +4,8 @@ import pandas
 import sqlite3
 from math import sqrt
 
+
+
 print("Ingresa los valores del punto 1")
 xl = float(input())
 y1 = float(input())
@@ -45,3 +47,30 @@ y2 = float(input("Introduce y2: "))
 distancia = calcular_distancia(x1, y1, x2, y2)
 
 print(f"""La distancia entre los puntos ({x1}, {y1}) y ({x2}, {y2}) es: {distancia}""")
+
+
+
+#ALTER TABLE airports_data
+# ADD Distancia_KM;
+#UPDATE airports_data
+#SET Distancia_KM = (
+#    --CASE WHEN strftime('%m%d', CURRENT_DATE) >= strftime('%m%d', Fecha_Nacimiento)
+#    --    THEN strftime('%Y', CURRENT_DATE) - strftime('%Y', Fecha_Nacimiento)
+#    --    ELSE strftime('%Y', CURRENT_DATE) - strftime('%Y', Fecha_Nacimiento) - 1
+#    --END
+#	--Distancia = Velocidad * Tiempo
+#	Distancia = range * scheduled_arrival
+#); 
+
+#SELECT * FROM airports_data;
+
+
+
+
+
+conn = sqlite3.connect("data/travel.sqlite")
+
+#Consulta de los aeropuertos 
+consulta_datos = "SELECT * FROM flights INNER JOIN aircrafts_data ON flights.aircraft_code = aircrafts_data.aircraft_code INNER JOIN airports_data ON flights.arrival_airport = airports_data.airport_code;"
+Df_aiports_data   = pd.read_sql_query(sql = consulta_datos, con = conn)
+Df_aiports_data

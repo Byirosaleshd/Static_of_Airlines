0fd8b9f58a95a08363226863f94b926da986f762
Byirosaleshd <68984100+Byirosaleshd@users.noreply.github.com>
2024-06-13 17:48:07 -0400

    Funciones para la pregunta A
    -Fueron creadas dos funciones para la pregunta A
    -Fue eliminado el archivo ipynb de la pregunta A ya que no es pertinente
    -Agregadas Respuesta de Pregunta A al informe.

--- Analisis/Functions.py
+++ Analisis/Functions.py
@@ -1,8 +1,17 @@
-import pandas as pd 
+import streamlit as st
+import datetime as dt
+import pandas    as pd
+import seaborn   as sns
+import matplotlib.pyplot as plt
+import sqlite3 as sql
+import numpy as np
+import Functions as ft
+import datetime as dt
+from pandas import json_normalize
+import json
 
 def read_abilities(query:str, conector) -> pd.DataFrame:
     """"Esta funcion crea el Dataframe que se requiere para el ejemplo """
-
     Df_aircrafts_data   = pd.read_sql_query(sql = query, con = conector)
     return Df_aircrafts_data
 
@@ -10,4 +19,36 @@ def read_abilities(query:str, conector) -> pd.DataFrame:
 def load_view_to_dataframe(view_name:str) -> pd.DataFrame:
     query = f"SELECT * FROM {view_name}"
     return pd.read_sql_query(query, conn)
+
+
+def procesar_vuelos(Df, columna_salida, columna_llegada):
+    Df[columna_salida] = pd.to_datetime(Df[columna_salida])
+    Df[columna_llegada] = pd.to_datetime(Df[columna_llegada])
+    Df['hora de salida'] = Df[columna_salida].apply(lambda x: x.hour + x.minute / 60)
+    Df['hora de llegada'] = Df[columna_llegada].apply(lambda x: x.hour + x.minute / 60)
+    Df['duracion_vuelo'] = Df['hora de salida'] - Df['hora de llegada']
+    modelo_tiempo_promedio = Df.groupby('model')['duracion_vuelo'].mean()
+    modelo_tiempo_promedio = modelo_tiempo_promedio.sort_values(ascending=True)
+    modelo_menor_tiempo = modelo_tiempo_promedio.index[0]
+    return modelo_menor_tiempo
+
+
+def modelo_frecuente(Df, columna_salida):    
+    modelo_vuelos = Df.groupby('model')[columna_salida].count()
+    modelo_vuelos = modelo_vuelos.sort_values(ascending=False)
+    modelo_mas_frecuente = modelo_vuelos.index[0]
+    return modelo_mas_frecuente
+
+
+
+
+def caracteristicas_modelo(Df, columna_salida, columna_llegada,nombres):
+    modelo_menor_tiempo = procesar_vuelos(Df, columna_salida, columna_llegada)
+    modelo_mas_frecuente = modelo_frecuente(Df, columna_salida)
+    st.write(f"El modelo de avión que realiza vuelos en el menor tiempo promedio es: {modelo_menor_tiempo}")
+    st.write(f"El modelo de avión que realiza la mayor cantidad de vuelos es: {modelo_mas_frecuente}")
+    Df[columna_salida] = pd.to_datetime(Df[columna_salida]).dt.time
+    Df[columna_llegada] = pd.to_datetime(Df[columna_llegada]).dt.time
+    Df = Df[[columna_salida,'hora de salida',columna_llegada,'hora de llegada','departure_airport','arrival_airport','status','aircraft_code','Nombre en ingles','Nombre en ruso','duracion_vuelo']]
+    st.write(Df)
 
--- Analisis/app.py
+++ Analisis/app.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import sqlite3 as sql
 import numpy as np
 import Functions as ft
+import datetime as dt
+from pandas import json_normalize
+import json
 
 # Título de la aplicación
 st.title("Determinar el mejor modelo de avión para vuelos más eficientes en distintos aeropuertos")
@@ -26,6 +29,8 @@ E.	Entre los modelos de aviones con los códigos: CR2, 733 y CN1 se desea conoce
 # Cargar la base de datos
 conn = sql.connect('../.data/travel.sqlite') 
 
+st.write('# ¿Qué modelo de avión realiza una mayor cantidad de vuelos, y cuál lo hace en un menor tiempo?')
+
 Pregunta_A = """SELECT flights.flight_id,
 flights.flight_no,
 flights.scheduled_departure,
@@ -34,44 +39,17 @@ flights.departure_airport,
 flights.arrival_airport,
 flights.status,
 flights.aircraft_code,
-aircrafts_data.model
+aircrafts_data.model,
+json_extract(aircrafts_data.model, '$.en') AS 'Nombre en ingles',
+json_extract(aircrafts_data.model, '$.ru') AS 'Nombre en ruso'
 FROM flights
 INNER JOIN aircrafts_data 
 ON aircrafts_data.aircraft_code = flights.aircraft_code;"""
 
-Df_vista1 = ft.read_abilities(Pregunta_A,conn)
 
-Df_vista2 = Df_vista1[['scheduled_departure','scheduled_arrival','departure_airport','arrival_airport','status','aircraft_code','model']]
-Df_vista2['scheduled_departure'] = pd.to_datetime(Df_vista2['scheduled_departure']).dt.time
-Df_vista2['scheduled_arrival'] = pd.to_datetime(Df_vista2['scheduled_arrival']).dt.time
-Df_vista2
-# Agrupamos por modelo y contamos la cantidad de vuelos
-modelo_vuelos = Df_vista2.groupby('model')['scheduled_departure'].count()
+Df_PreguntaA = ft.read_abilities(Pregunta_A,conn)
+ft.caracteristicas_modelo(Df_PreguntaA, 'scheduled_departure', 'scheduled_arrival','model')
 
-# Ordena en orden descendente
-modelo_vuelos = modelo_vuelos.sort_values(ascending=False)
-
-# Obtiene el modelo con la mayor cantidad de vuelos
-modelo_mas_frecuente = modelo_vuelos.index[0]
-
-print(f"El modelo de avión que realiza la mayor cantidad de vuelos es: {modelo_mas_frecuente}")
-# Extraer horas y minutos para transformar todo en horas
-Df_vista2['hora de salida'] = Df_vista2['scheduled_departure'].apply(lambda x: x.hour + x.minute / 60)
-Df_vista2['hora de llegada'] = Df_vista2['scheduled_arrival'].apply(lambda x: x.hour + x.minute / 60)
-# Calcula la duración de cada vuelo
-Df_vista2['duracion_vuelo'] = Df_vista2['hora de salida'] - Df_vista2['hora de llegada'] 
-
-# Agrupa por modelo y calcula el tiempo promedio de vuelo
-modelo_tiempo_promedio = Df_vista2.groupby('model')['duracion_vuelo'].mean()
-
-# Ordena en orden ascendente
-modelo_tiempo_promedio = modelo_tiempo_promedio.sort_values(ascending=True)
-
-# Obtiene el modelo con el menor tiempo promedio de vuelo
-modelo_menor_tiempo = modelo_tiempo_promedio.index[0]
-
-st.write(modelo_menor_tiempo)
-st.write(f"El modelo de avión que realiza vuelos en el menor tiempo promedio es: {modelo_menor_tiempo}")
 
 # Mostrar el DataFrame
 #st.write("Datos de la base de datos:")

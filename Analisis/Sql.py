ef441d17bcd679496aaa293db225f2e759b495ee
Shumagito <161162742+Shumagito@users.noreply.github.com>
2024-06-21 20:08:07 -0400

    Solucion de la vista E2
    - Solucione la dependencia de la vista E2 con una subconsulta de SQL

--- Analisis/Sql.py
+++ Analisis/Sql.py
@@ -86,12 +86,32 @@ ORDER BY
     
     
 
-PreguntaD2= """ 
-SELECT
+PreguntaD2= """ SELECT
     arr_city AS asian_city,
     COUNT(flight_no) AS num_flights
 FROM
-    European_To_Asian_Flights
+    (SELECT
+    flights.flight_no,
+    flights.departure_airport,
+    flights.arrival_airport,
+    dep_airports.city AS dep_city,
+    arr_airports.city AS arr_city,
+    dep_airports.timezone AS dep_timezone,
+    arr_airports.timezone AS arr_timezone
+FROM
+    flights
+INNER JOIN
+    airports_data AS dep_airports
+ON
+    flights.departure_airport = dep_airports.airport_code
+INNER JOIN
+    airports_data AS arr_airports
+ON
+    flights.arrival_airport = arr_airports.airport_code
+WHERE
+    dep_airports.timezone LIKE 'Europe/%'
+AND
+    arr_airports.timezone LIKE 'Asia/%')
 GROUP BY
     arr_city
 ORDER BY
